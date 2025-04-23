983c18cb25ba43e908fd2e97928b73f67c217b28
arifulislamk <ariful18374@gmail.com>
2025-04-23 22:23:59 +0600

    slider design

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,10 +3,10 @@ import AwesomeSlider from "react-awesome-slider";
 import "react-awesome-slider/dist/styles.css";
 import { Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/pagination";
 
-import './styles.css';
+import "./styles.css";
 const Banner = () => {
   return (
     <div>
@@ -19,29 +19,50 @@ const Banner = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide ><div className="w-1/2 rounded-lg  ">
-        <img className=" rounded-lg" src="https://i.ibb.co.com/N6LGVTb/IMG20241122173422.jpg" alt="f" /></div>
+        <SwiperSlide className=" gap-3" >
+        <img
+              src="https://i.ibb.co.com/fY9ZH4KW/miami-black-font.jpg"
+              alt="f"
+            />
+            <img
+              src="https://i.ibb.co.com/VcGpdxqh/miami-black-back.jpg"
+              alt="f"
+            />
+         
         </SwiperSlide>
-        <SwiperSlide ><div className="w-1/2 rounded-md">
-        <img src="https://i.ibb.co.com/N6LGVTb/IMG20241122173422.jpg" alt="f" /></div>
+        <SwiperSlide className=" gap-3">
+            <img className=" rounded-md " src="https://i.ibb.co.com/8ndPZ7vX/miami.jpg" alt="f" />
+            <img src="https://i.ibb.co.com/8ndPZ7vX/miami.jpg" alt="f" />
         </SwiperSlide>
-        <SwiperSlide ><div className="w-1/2 rounded-md">
-        <img src="https://i.ibb.co.com/N6LGVTb/IMG20241122173422.jpg" alt="f" /></div>
+        <SwiperSlide className=" gap-3">
+            <img src="https://i.ibb.co.com/Y4RxZqpG/real-fonts.jpg" alt="f" />
+            <img src="https://i.ibb.co.com/Q37gKdcR/real-font.jpg" alt="f" />
         </SwiperSlide>
-        <SwiperSlide ><div className="w-1/2 rounded-md">
-        <img src="https://i.ibb.co.com/N6LGVTb/IMG20241122173422.jpg" alt="f" /></div>
+        
+        <SwiperSlide className=" gap-3 rounded-md">
+        <img src="https://i.ibb.co.com/hFypbCWg/bayers-font.jpg" alt="f" />
+            <img src="https://i.ibb.co.com/TMgSPdqd/bayaer-read.jpg" alt="f" />
+   
         </SwiperSlide>
-        <SwiperSlide ><div className="w-1/2 rounded-md">
-        <img src="https://i.ibb.co.com/N6LGVTb/IMG20241122173422.jpg" alt="f" /></div>
+        <SwiperSlide>
+          <div className="w-1/2 rounded-lg  ">
+            <img
+              className=" rounded-lg"
+              src="https://i.ibb.co.com/SD7FYMNt/Al-naser.jpg
+"
+              alt="f"
+            />
+          </div>
         </SwiperSlide>
-        <SwiperSlide ><div className="w-1/2 rounded-md">
-        <img src="https://i.ibb.co.com/N6LGVTb/IMG20241122173422.jpg" alt="f" /></div>
+        <SwiperSlide>
+          <div className="w-1/2 rounded-md">
+            <img src="https://i.ibb.co.com/wFmvHWjd/chelse-font.jpg" alt="f" />
+          </div>
         </SwiperSlide>
-        <SwiperSlide ><div className="w-1/2 rounded-md">
-        <img src="https://i.ibb.co.com/N6LGVTb/IMG20241122173422.jpg" alt="f" /></div>
-        </SwiperSlide>
-        <SwiperSlide ><div className="w-1/2 rounded-md">
-        <img src="https://i.ibb.co.com/N6LGVTb/IMG20241122173422.jpg" alt="f" /></div>
+        <SwiperSlide>
+          <div className="w-1/2 rounded-md">
+            <img src="https://i.ibb.co.com/21PN39yc/man-uni.jpg" alt="f" />
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Banner from "../components/Banner";
 const Home = () => {
     return (
         <div>
-            <h2>Home page</h2>
             <Banner />
         </div>
     );
