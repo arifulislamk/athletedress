ae4e7f73ea55f60165eb5c1c7c416a68ad33ac7d
arifulislamk <ariful18374@gmail.com>
2025-05-04 07:17:55 +0600

    login submit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,25 @@
+import { useForm } from "react-hook-form";
+
 const Login = () => {
+  const { register, handleSubmit} = useForm() ;
+  const handleLoginBtn = data => {
+    const { email, password} = data ;
+    console.log(email,password);
+  }
   return (
     <div className=" justify-center flex">
       <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-50 dark:text-gray-800">
         <h1 className="text-2xl font-bold text-center">Login</h1>
-        <form noValidate="" action="" className="space-y-6">
+        <form onSubmit={handleSubmit(handleLoginBtn)} noValidate="" action="" className="space-y-6">
           <div className="space-y-1 text-sm">
             <label htmlFor="username" className="block dark:text-gray-600">
-              Username
+              Email
             </label>
             <input
-              type="text"
-              name="username"
-              id="username"
-              placeholder="Username"
+              type="email"
+              { ...register("email")}
+              name="email"
+              placeholder="Email"
               className="w-full px-4 py-3 rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600"
             />
           </div>
@@ -22,8 +29,8 @@ const Login = () => {
             </label>
             <input
               type="password"
+              {...register("password")}
               name="password"
-              id="password"
               placeholder="Password"
               className="w-full px-4 py-3 rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600"
             />
